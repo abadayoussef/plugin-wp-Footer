933a7e0f6bda5ceac941bcd874390394e3da2b0c
ahmedBou <instinctauto@gmail.com>
2020-06-21 14:53:54 +0100

    fill the form with pre defined data from db in the sitting

--- abufooter/admin/partials/update-footer.php
+++ abufooter/admin/partials/update-footer.php
@@ -1,21 +1,26 @@
+<?php
+    global $wpdb;
+    require_once plugin_dir_path(__FILE__) . '../../includes/class-abufooter-activator.php';
+        $rows = $wpdb->get_results("SELECT * FROM ". Abufooter_Activator::abuFooter_table() ,ARRAY_A);
+    ?>
 <div class="container">
   <h1>Footer setting</h1>
   <form action="javascript:void(0)" id="formFooter">
   <div class="form-group">
         <label for="ftext">Footer text:</label>
-        <textarea name="footer_text" class="form-control" rows="5" id="ftext" placeholder="Enter footer text" required></textarea>
+        <textarea name="footer_text" class="form-control" rows="5" id="ftext" placeholder="Enter footer text" required><?php echo $rows[0]["footer_text"]?></textarea>
     </div>
     <div class="form-group">
         <label for="fface">facebook</label>
-        <input name="facebook" type="text" class="form-control" id="fface" placeholder="Enter facebook url" required>
+        <input name="facebook" value="<?php echo $rows[0]["f_facebook"]?>" type="text" class="form-control" id="fface" placeholder="Enter facebook url" required>
     </div>
     <div class="form-group">
         <label for="ftwitter">Twitter</label>
-        <input name="twitter" type="text" class="form-control" id="ftwitter" placeholder="Enter twitter url" required>
+        <input name="twitter" value="<?php echo $rows[0]["f_twitter"]?>" type="text" class="form-control" id="ftwitter" placeholder="Enter twitter url" required>
     </div>
     <div class="form-group">
         <label for="fcoord">coordinate</label>
-        <input name="coordinate" type="text" class="form-control" id="fcoord" placeholder="Enter coordinate" required>
+        <input name="coordinate" value="<?php echo $rows[0]["f_email"]?>" type="text" class="form-control" id="fcoord" placeholder="Enter coordinate" required>
     </div>
 
     <button class="btn btn-primary" type="submit">Update Footer</button>
